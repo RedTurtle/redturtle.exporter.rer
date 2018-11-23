65bd42c18b444236f286462376ca5eb0151d6f30
Andrea Cecchi <andrea.cecchi85@gmail.com>
2018-11-23 08:07:25 +0100

    Back to development: 0.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,12 @@ Changelog
 
 
 
+0.1.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.1.0 (2018-11-23)
 ------------------
 - Exported Geolocation object info.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='redturtle.exporter.rer',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description="A JSON exporter from RER",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
